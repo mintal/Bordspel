2a9c9a4f914e23f1ee65d64f8b9b6053be01d0c4
Matthijs Boelens <matthijs1995@hotmail.com>
2017-05-08 14:18:48 +0200

    Initialized SRC folder

---
+++ src/com/thehobbit/game/Engine.java
@@ -0,0 +1,12 @@
+package com.thehobbit.game;
+
+/**
+ * Created by mintal on 08/05/2017.
+ */
+public class Engine {
+
+    public static void main(String[] args) {
+        System.out.println("Starting game engine.");
+    }
+
+}
